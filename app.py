ee7d00d0c8757333dba17120f45f4592035bfb2a
brysonmealer2021-cmd <brysonmealer2021@gmail.com>
2026-03-14 00:56:39 -0400

    Update app.py

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ if not st.session_state["authenticated"]:
     
     if st.button("Open the Ledger"):
         res = supabase.table("users").select("*").eq("username", user_input).eq("pin", pin_input).execute()
-                if user_input == "YourName" and pin_input == "1580":
+                if user_input == "Bryce" and pin_input == "1580":
             st.session_state["authenticated"] = True
             st.session_state["username"] = "Bryce"
             st.session_state["role"] = "manager"
